cd5b32018062a6f5fabba4df3f2d35919e5c0638
anthonypillalaza <alexanderpillalaza@gmail.com>
2026-01-11 12:02:28 -0500

    el boton modifica de la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,5 +1,7 @@
 saludar=function(){
-console.log(" ingresa a saludar");
-
-
+    let cmpInagenSaludo
+    cmpInagenSaludo=document.getElementById("imgSaludo");
+    cmpInagenSaludo.src="./imagenes/200.gif"
+    console.log(" ingresa a saludar");
+    
 }
