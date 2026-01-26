5ad509f6c80d1b37510fa9f91ca94b801754a770
anthonypillalaza <alexanderpillalaza@gmail.com>
2026-01-25 19:52:47 -0500

    agregar carpeta bucles

---
+++ bucles/ejemploFor.js
@@ -0,0 +1,63 @@
+mostrarNumeros = function () {
+    console.log("antes del for")
+
+    for (let i = 0; i < 4; i++) {
+        console.log(i);
+    }
+
+    console.log("despues del for")
+
+}
+
+
+mostrarNumeros2 = function () {
+    console.log("antes del for")
+
+    for (let indice = 0; indice <= 5; indice++) {
+        console.log(indice);
+    }
+
+    console.log("despues del for")
+
+}
+
+
+mostrarPares = function () {
+    console.log("antes del for")
+
+    for (let x=2; x <= 10; x+=2) {
+        console.log(x);
+    }
+
+    console.log("despues del for")
+
+}
+
+mostrarInverso = function () {
+    console.log("antes del for")
+
+    for (let i=10; i>=0; i--) {
+        console.log(i);
+    }
+
+    console.log("despues del for")
+
+}
+hackearNasaPelis=function(){
+
+    for(let porcentaje=0;porcentaje<=100;porcentaje+=10){
+        console.log("kackeando nasa"+porcentaje+"%");
+    }
+    console.log("La Nasa ha sido hackeada");
+}
+
+mostrarImpares = function () {
+    console.log("antes del for")
+
+    for (let s=1; s <= 21; s+=2) {
+        console.log(s);
+    }
+
+    console.log("despues del for")
+
+}
