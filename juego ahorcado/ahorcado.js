479b6377d64ba60d965ed9d56d41fee6e33973db
anthonypillalaza <alexanderpillalaza@gmail.com>
2026-02-01 13:48:39 -0500

    paso 1

--- juego ahorcado/ahorcado.js
+++ juego ahorcado/ahorcado.js
@@ -1,7 +1,36 @@
 
- //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
-esMayuscula=fuction(caracter){
+//No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta;
+let intentos = 0;
+let coincidencias = 0;
+let errores = 0;
 
-
+// PASO 0: Validar si es mayúscula usando ASCII
+esMayuscula = function (caracter) {
+    let codigo = caracter.charCodeAt(0);
+    if (codigo >= 65 && codigo <= 90) {
+        return true;
+    } else {
+        return false;
+    }
 }
-   
+
+
+guardarPalabra = function () {
+    let input = document.getElementById("txtSecreta");
+    let valor = input.value;
+    let esValida = true;
+
+    if (valor.length == 5) {
+        esValida = false;
+        console.log("no")
+    } else {
+        for (let i = 0; i < valor.length; i++) {
+            if (!esMayuscula(valor.charAt(i))) {
+                esValida = false;
+                break;
+            
+            }
+        }
+    }
+}
