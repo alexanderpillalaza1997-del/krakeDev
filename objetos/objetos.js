c92ee81de3bdb9ad641e89aba7112f9e4e241837
anthonypillalaza <alexanderpillalaza@gmail.com>
2026-02-08 16:24:32 -0500

    agregar objetos

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -37,8 +37,59 @@ crearProducto=function(){
         console.log("producto2 es mayor ")
     } else {
         console.log(" ambos productos tienen el mismo stock ")
-    }
-
-    
+    }   
 
 }
+ modificarAtributos=function(){
+    let cuenta={
+        numero: "5323423423",
+        saldo: 0.0
+    }
+    cuenta.saldo=100;
+    cuenta.saldo+=10;
+    console.log(cuenta.saldo)
+ }
+
+ crearCliente= function(){
+    let cliente={
+        cedula: "171231",
+        nomnbre: "Juan"
+    }
+    let cliente1={};
+    cliente1.nombre="Romeo";
+    cliente1.apellido="Salcedo";
+    cliente1.cedula="123123";
+
+ }
+
+ probarIncrementoSaldo=function(){
+    let cta={numero:"123114", saldo:34.00}
+    IncrementarCuenta(cta,100);
+    console.log(cta.saldo)
+ }
+
+ probarDeterminarMayor=function(){
+    let per1={nombre:"Daniel", edad:45} ;
+    let per2={nombre:"Luisa", edad:48};
+    let mayor;
+    mayor=determinarMayor(per1,per2);
+    if (mayor != null ){
+        console.log("El mayor es: " +mayor.nombre)
+    }
+ }
+
+ IncrementarCuenta=function(cuenta,monto){
+    cuenta.saldo+=monto;
+
+ }
+
+ determinarMayor=function(persona1, persona2){
+    if (persona1.edad > persona2.edad){
+        return persona1;
+    } else if (persona2.edad> persona1.edad){
+        return persona2;
+    }else {
+        return null;
+    }
+ }
+
